bc9cf20f37a01592b5ab744b30f6aa93f6c4623b
Ali Naghizadeh <ali_naghizadeh@yahoo.com>
2021-08-03 19:17:03 +0430

    load F_TO_E_Provice.xlsx and F_TO_E_County.xlsx files

--- Data/County_Division/Cleaned Data/Final Cleaning.R
+++ Data/County_Division/Cleaned Data/Final Cleaning.R
@@ -3,3 +3,9 @@ library(readxl)
 library(writexl)
 library(tidyverse)
 library(stringi)
+
+# load F_TO_E_Provice.xlsx and F_TO_E_County.xlsx files
+
+FTOEP<- read_xlsx("G:/Github/SEZ-Internal-Migration/Data/County_Division/Cleaned Data/F_To_E_Province.xlsx")
+
+FTOEC <- read_xlsx("G:/Github/SEZ-Internal-Migration/Data/County_Division/Cleaned Data/F_To_E_County.xlsx")
