f093c457766d66d8eb5b6adba165a81cf0997c4b
Ali Naghizadeh <ali_naghizadeh@yahoo.com>
2021-08-20 13:32:47 +0430

    Create Transition Data set with County (each Year To 81)

--- Data/County_Division/Cleaned Data/Transition.R
+++ Data/County_Division/Cleaned Data/Transition.R
@@ -10,9 +10,9 @@ library(readxl)
 
 Diff_Sheet <- excel_sheets("Data/County_Division/Cleaned Data/Diff_County.xlsx")
 
-Transition <- read_xlsx("Data/County_Division/Cleaned Data/Final_County_ID.xlsx",
+Transition_I <- read_xlsx("Data/County_Division/Cleaned Data/Final_County_ID.xlsx",
                         sheet = "98")
-Transition <- Transition%>%
+Transition_I <- Transition_I%>%
   select(County_ID)
 
 for (i in 1:(length(Diff_Sheet)-1)) {
@@ -23,16 +23,16 @@ for (i in 1:(length(Diff_Sheet)-1)) {
     select(County_ID,Previous_C_ID)%>%
     mutate_all(as.character)
   
-  Transition <- Transition%>%
+  Transition_I <- Transition_I%>%
     left_join(DiFF,by = "County_ID")%>%
     mutate(Previous_C_ID = ifelse(is.na(Previous_C_ID),County_ID,Previous_C_ID))
   
-  colnames(Transition)[colnames(Transition) == "County_ID"] <- paste0("CID_",County_Sheet[i])
-  colnames(Transition)[colnames(Transition) == "Previous_C_ID"] <- "County_ID"
+  colnames(Transition_I)[colnames(Transition_I) == "County_ID"] <- paste0("CID_",County_Sheet[i])
+  colnames(Transition_I)[colnames(Transition_I) == "Previous_C_ID"] <- "County_ID"
   
 }
 
-Transition <- Transition%>%
+Transition_I <- Transition_I%>%
     mutate(Equal = case_when(
         CID_98 == CID_97 & CID_97 == CID_96 & CID_96 == CID_95 & CID_95 == CID_94 & 
         CID_94 == CID_93 & CID_93 == CID_92 & CID_92 == CID_91 & CID_91 == CID_90 & 
@@ -41,23 +41,84 @@ Transition <- Transition%>%
         TRUE ~ 0
     ))
 
-New_County <- Transition%>%
+New_County_I <- Transition_I%>%
   filter(Equal == 0)
 
 Year <- c("98","97","96","95","94","93","92","91","90","89","88","85","82","81")
 
-TR_to_81 <- tibble()
+TRTo81_I <- tibble()
   
 for (i in 1:13) {
   
-  TR <- Transition%>%
+  TR <- Transition_I%>%
     select(eval(i),14)%>%
     mutate(Year = Year[i])
   
   colnames(TR)[1] <- "C_ID"
   colnames(TR)[colnames(TR) == "County_ID"] <- "CID_81"
   
-  TR_to_81 <- bind_rows(TR_to_81,TR)
+  TRTo81_I <- bind_rows(TRTo81_I,TR)
+}
+
+################################################################################
+                  # Transition Data set with County  #
+################################################################################
+
+Diff_Sheet <- excel_sheets("Data/County_Division/Cleaned Data/Diff_County.xlsx")
+
+Transition_C <- read_xlsx("Data/County_Division/Cleaned Data/Final_County_ID.xlsx",
+                        sheet = "98")
+Transition_C <- Transition_C%>%
+  select(County)
+
+for (i in 1:(length(Diff_Sheet)-1)) {
+  
+  DiFF <- read_xlsx("Data/County_Division/Cleaned Data/Diff_County.xlsx",
+                    sheet = Diff_Sheet[i])
+  DiFF <- DiFF%>%
+    select(County,Previous_County)%>%
+    mutate_all(as.character)
+  
+  Transition_C <- Transition_C%>%
+    left_join(DiFF,by = "County")%>%
+    mutate(Previous_County = ifelse(is.na(Previous_County),County,Previous_County))
+  
+  colnames(Transition_C)[colnames(Transition_C) == "County"] <- paste0("County_",County_Sheet[i])
+  colnames(Transition_C)[colnames(Transition_C) == "Previous_County"] <- "County"
+  
 }
 
 
+Transition_C <- Transition_C%>%
+  mutate(Equal = case_when(
+      County_98 == County_97 & County_97 == County_96 & County_96 == County_95 & County_95 == County_94 & 
+      County_94 == County_93 & County_93 == County_92 & County_92 == County_91 & County_91 == County_90 & 
+      County_90 == County_89 & County_89 == County_88 & County_88 == County_85 & County_85 == County_82 &
+      County_82 == County ~ 1,
+    TRUE ~ 0
+  ))
+
+New_County_C <- Transition_C%>%
+  filter(Equal == 0)
+
+Year <- c("98","97","96","95","94","93","92","91","90","89","88","85","82","81")
+
+TRTo81_C <- tibble()
+
+for (i in 1:13) {
+  
+  TR <- Transition_C%>%
+    select(eval(i),14)%>%
+    mutate(Year = Year[i])
+  
+  colnames(TR)[colnames(TR) == "County"] <- "County_81"
+  colnames(TR)[1] <- "County"
+  
+  
+  TRTo81_C <- bind_rows(TRTo81_C,TR)
+}
+
+A <- TRTo81_C%>%
+  distinct(County,County_81)
+B <- TRTo81_I%>%
+  distinct(C_ID,CID_81)
